a1fe26e1d10f9c40134f88d35be395fad33d9fec
Elli K <kauhanen.elli@gmail.com>
2022-06-22 19:00:41 +0300

    updates to gh pages www

--- Matkakertomus/matkakertomus/src/index.js
+++ Matkakertomus/matkakertomus/src/index.js
@@ -5,6 +5,7 @@ import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {HashRouter} from 'react-router-dom'
+// Site hosted at GitHub pages
 
 ReactDOM.render(
   <React.StrictMode>
